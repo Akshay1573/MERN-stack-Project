b0b804d42880cbce46f03e967ba383a1eb0bfcf4
Akshay <suryawanshi9322akshay@gmail.com>
2024-09-26 17:34:21 +0530

    connection of mongodb

---
+++ api/config/db.config.js
@@ -0,0 +1,6 @@
+const dbConfig = {
+    DB_NAME: "Mern-Blog",
+    DB_URL: "mongodb://localhost:27017/"
+};
+
+export default dbConfig;
--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-const express = require("express")
+import express from "express";
+import mongoose from "mongoose";
+import dbConfig from "./config/db.config.js";
 
-const app = express();
 
-app.listen(3000, ()=>{
-    console.log('server is running on port 3000!!');
+mongoose
+  .connect(dbConfig.DB_URL)
+  .then(() => {
+    console.log("mongoDb is connected");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
+
+  const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000!!");
 });
